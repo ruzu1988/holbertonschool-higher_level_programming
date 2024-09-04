d30d210837c2eeade5c1cb133dd2bdf36a9e0d22
Zuleyka Rubio <zrubio4898@gmail.com>
2024-09-04 19:35:14 -0400

    concat edges fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming"\
-    "language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[105:111] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with Python"
+print(str)
