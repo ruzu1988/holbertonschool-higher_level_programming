59d1c8afb3f780e728c37ea5171d84c916b5ea5e
Zuleyka Rubio <zrubio4898@gmail.com>
2024-09-05 22:34:25 -0400

    .

--- python-if_else_loops_functions/ 5-print_comb2.py
+++ python-if_else_loops_functions/ 5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-for i in range(100):
-    if i == 99:
-        print("{:02d}".format(i))
-    else:
-        print("{:02d}".format(i), end=", ")
-        
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+        else:
+            print("{}{}".format(num1, num2), end=", ")
