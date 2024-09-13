7a481cca9a6391792caecb4fc42605a9a66b06dc
Zuleyka Rubio <zrubio4898@gmail.com>
2024-09-12 23:19:15 -0400

    best score fixed

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,6 +5,7 @@ def best_score(a_dictionary):
     best_key = None
     max_value = float('-inf')
     for key, value in a_dictionary.items():
-        max_value = value
-        best_key = key
+        if value > max_value:
+            max_value = value
+            best_key = key
     return best_key
