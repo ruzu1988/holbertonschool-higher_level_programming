9baa1272a750831c2538d3fb1d795fcf7d7120a0
Zuleyka Rubio <zrubio4898@gmail.com>
2024-09-13 12:42:10 -0400

    fixed code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for idx in range(x):
         try:
-            print(my_list[idx], end="")
+            print("{}".format(my_list[idx]), end="")
             count += 1
-        except: continue
+        except: 
+            continue
     print()
     return count
