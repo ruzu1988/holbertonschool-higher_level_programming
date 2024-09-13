81cf3ed358eae484b618fb6aff34a9bb92530866
Zuleyka Rubio <zrubio4898@gmail.com>
2024-09-12 22:14:13 -0400

    changes code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     return [[element ** 2 for element in row] for row in matrix]
-matrix = [[1, 2, 3], [4, 5, 6]]
-square_matrix = square_matrix_simple(matrix)
-print(square_matrix)
