a54e99c16fec7dc6c0fa85de711b964037795577
Zuleyka Rubio <zrubio4898@gmail.com>
2024-10-21 17:13:38 -0400

    fizzbuzz fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,6 +5,7 @@ def fizzbuzz():
             print("Fizzbuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz0", end=" ")
         else:
             print(i, end=" ")
-    
