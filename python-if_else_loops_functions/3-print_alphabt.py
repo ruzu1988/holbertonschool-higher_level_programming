25f7f229fca98403aff5683cdb54f7befa73de62
Zuleyka Rubio <zrubio4898@gmail.com>
2024-10-20 21:19:43 -0400

    print alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for upper_ASCII in range(65, 90):
-    if upper_ASCII != 101 and upper_ASCII != 113:
-        print("{}".format(chr(upper_ASCII)), end="")
+for i in range(97, 123):
+    if i not in (101, 113):
+        print("{}".format(chr(i)), end="")
