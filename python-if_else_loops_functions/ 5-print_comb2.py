2eb87bd58f44fe2ea1863f2cf86ec365b9c7be23
Zuleyka Rubio <zrubio4898@gmail.com>
2024-10-21 15:15:35 -0400

    5 print fixed

--- python-if_else_loops_functions/ 5-print_comb2.py
+++ python-if_else_loops_functions/ 5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for idx in range(0, 100):
-    if idx < 99:
-        print("{:02d}, ".format(idx), end="")
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
     else:
-        print("{}".format(int(idx)))
+        print("{}".format(int(i)))
     
