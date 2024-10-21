3bc970ecbec8aa85faa76245b135565aee689554
Zuleyka Rubio <zrubio4898@gmail.com>
2024-10-21 16:00:27 -0400

    .

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def print_last_digit(number):
     lastdg = abs (number) % 10 #-obtiene el valor absoluto del # -no afecta si es neg
     print("{}".format(lastdg), end="")
